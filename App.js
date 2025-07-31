0584ed665c72d01372f0691fefb7c7a848a4c262
Davidson Fernandes dos Reis Silva <fdavidson991@gmail.com>
2025-07-31 16:25:25 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Cadastro from './components/Cadastro';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login.jsx';
+import Cadastro from './components/Cadastro';  // cuidado com o "C" maiúsculo
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/cadastro" element={<Cadastro />} />
-    </Routes>
+    <Router>
+      <Routes>
+        {/* Defina a rota padrão para Login */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
+        {/* Redireciona "/" para "/login" */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Opcional: rota para páginas não encontradas */}
+        <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
