d0c69df2a5820b852861a4f658f1838ce9573fd4
Davidson Fernandes dos Reis Silva <fdavidson991@gmail.com>
2025-08-03 21:12:29 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login.jsx';
-import Cadastro from './components/Cadastro';  // cuidado com o "C" maiúsculo
+import { Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Cadastro from '../pages/Cadastro';
+import RecuperarSenha from '../pages/RecuperarSenha';  // importe o componente
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        {/* Defina a rota padrão para Login */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-
-        {/* Redireciona "/" para "/login" */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* Opcional: rota para páginas não encontradas */}
-        <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/recuperar-senha" element={<RecuperarSenha />} /> {/* rota para recuperação */}
+    </Routes>
   );
-}
+};
 
 export default App;
